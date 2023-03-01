7c23320a505154ff82f2fe568a67ba9c44d21052
roshan <roshankaranth@gmail.com>
2023-03-02 00:25:55 +0530

    Fixed node_neighbor and wall_callback function

--- PlannerNode.py
+++ PlannerNode.py
@@ -4,13 +4,14 @@ from MapNode import MapNode
 
 
 class PlannerNode:
+    
     def __init__(self):
         self.current_obj=MapNode()
         # Since we know that the first step the bot will take will be down, we can simply do it here
         self.current_obj.direction_callback("down")  # example 1
 
         print(self.wall_callback())
-    
+
     @staticmethod
     def RetracePath(start_node,end_node):
          #Method to Retrace the path back to the start from the end
@@ -18,6 +19,7 @@ class PlannerNode:
          path=[]
          while(current_node.coords!=start_node.coords):
                coords_change = [current_node.coords[0]-current_node.parent.coords[0],current_node.coords[1]-current_node.parent.coords[1]]
+               
                if(coords_change[0]==1):
                    path.append(1)
                if(coords_change[0]==-1):
@@ -41,11 +43,11 @@ class PlannerNode:
              #Bottom wall absent
              node.neighbors.append(Node((node.coords[0]+1,node.coords[1]),self.current_obj._MapNode__map.array[node.coords[0]+1][node.coords[1]]))
              
-        if(node.value!=12 and node.value!=6 and node.value!=5 and node.value!=4 and node.value!=14 and node.value!=7 and node.value!=13 and node.value!=0):
+        if(node.value!=12 and node.value!=6 and node.value!=5 and node.value!=4 and node.value!=14 and node.value!=7 and node.value!=13 ):
              #Left wall absent
              node.neighbors.append(Node((node.coords[0],node.coords[1]-1),self.current_obj._MapNode__map.array[node.coords[0]][node.coords[1]-1]))
              
-        if(node.value!=2 and node.value!=3 and node.value!=10 and node.value!=6 and node.value!=14 and node.value!=7 and node.value!=11 and node.value!=0):
+        if(node.value!=2 and node.value!=3 and node.value!=10 and node.value!=6 and node.value!=14 and node.value!=7 and node.value!=11 ):
              #Right wall absent
              node.neighbors.append(Node((node.coords[0],node.coords[1]+1),self.current_obj._MapNode__map.array[node.coords[0]][node.coords[1]+1]))
 
@@ -53,6 +55,9 @@ class PlannerNode:
     def heuristic_cost(self,node_coords):
          #Determines the Manhattan distance between the current node and the end node
          return abs(node_coords[0]-self.current_obj._MapNode__map.end[0])+abs(node_coords[1]-self.current_obj._MapNode__map.end[1])
+       
+         #Euclidean distance
+         #return pow(pow(node_coords[0]-self.current_obj._MapNode__map.end[0],2) + pow(node_coords[1]-self.current_obj._MapNode__map.end[1],2),0.5)
 
 
     def wall_callback(self):
@@ -65,7 +70,7 @@ class PlannerNode:
             
             heapq.heapify(Open_Set)
             heapq.heappush(Open_Set,(start.fscore,count,start))
-            
+           
             while(len(Open_Set)!=0):
                  
                  current_Node = heapq.heappop(Open_Set)[2]
@@ -82,10 +87,15 @@ class PlannerNode:
                       temp_gscore = current_Node.gscore + 1
 
                       if(temp_gscore<neighbor.gscore):
+                           t=0
                            neighbor.parent = current_Node
                            neighbor.gscore = temp_gscore
                            neighbor.fscore = temp_gscore+ self.heuristic_cost(neighbor.coords)
-                           if(neighbor not in Open_Set):
+                           #To check weather neighbor node is already present in Open_Set
+                           for i in range(len(Open_Set)):
+                              if(neighbor.coords == Open_Set[i][2].coords):
+                                   t=1
+                           if(t==0):
                                 count+=1
                                 heapq.heappush(Open_Set,(neighbor.fscore,count,neighbor))
 
